16ff92b1e916ce60c0a612efcaa9a7588b3b9fab
David Herszenhaut <david.herszenhaut@gmail.com>
2021-07-16 18:13:06 -0400

    Added button for testing to show random integer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-// import { generateRandomIntegerInclusive } from './utils';
+import React, { useState } from 'react';
+import { generateRandomIntegerInclusive } from './utils';
 import './App.css';
 
 function App() {
+
+  const [randomInt, setRandomInt] = useState<number>(0);
+
   return (
     <div className="App">
       <header>rollgraph</header>
+      <button onClick={() => setRandomInt(generateRandomIntegerInclusive(1, 6))}>Test</button>
+      <p>{randomInt}</p>
     </div>
   );
 }
