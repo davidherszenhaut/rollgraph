b294d61981a58ac5c654fc854a073861e19389a1
David Herszenhaut <david.herszenhaut@gmail.com>
2021-07-17 16:33:47 -0400

    Added two number inputs for number of sides and number of rolls

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import './App.css';
 
 function App() {
 
-  const [randomInt, setRandomInt] = useState<number>(0);
+  const [faces, setFaces] = useState<string>('6');
+  const [times, setTimes] = useState<string>('1');
+  const [rolls, setRolls] = useState<number[]>([]);
+
+  function rollDice(faces: number, times: number) {
+    let rolls = [];
+    for (let i = 0; i < times; i++) {
+      rolls.push(generateRandomIntegerInclusive(1, faces));
+    }
+    console.log(rolls);
+    setRolls(rolls);
+  }
 
   return (
     <div className="App">
-      <header>🎲 rollgraph 📈</header>
-      <button onClick={() => setRandomInt(generateRandomIntegerInclusive(1, 6))}>Click Me!</button>
-      <p>{randomInt}</p>
+      <header>
+        <h1>🎲 rollgraph 📈</h1>
+      </header>
+      <p>I would like to roll a <input type="number" min="1" value={faces} onChange={e => setFaces(e.target.value)} />-sided die <input type="number" min="1" value={times} onChange={e => setTimes(e.target.value)} /> {times === "1" ? "time" : "times"}.</p>
+      <button onClick={() => rollDice(parseInt(faces), parseInt(times))}>Roll Dice!</button>
+      <p>{rolls.join(', ')}</p>
     </div>
   );
 }
