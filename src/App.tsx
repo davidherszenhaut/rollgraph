d7f46e3a046970ff302a7cbd8f325060ad4402d7
David Herszenhaut <david.herszenhaut@gmail.com>
2021-07-17 17:05:56 -0400

    Moved rollDice to utils and added tests for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { generateRandomIntegerInclusive } from './utils';
+import { rollDice } from './utils';
 import './App.css';
 
 function App() {
@@ -8,22 +8,13 @@ function App() {
   const [times, setTimes] = useState<string>('1');
   const [rolls, setRolls] = useState<number[]>([]);
 
-  function rollDice(faces: number, times: number) {
-    let rolls = [];
-    for (let i = 0; i < times; i++) {
-      rolls.push(generateRandomIntegerInclusive(1, faces));
-    }
-    console.log(rolls);
-    setRolls(rolls);
-  }
-
   return (
     <div className="App">
       <header>
         <h1>🎲 rollgraph 📈</h1>
       </header>
       <p>I would like to roll a <input type="number" min="1" value={faces} onChange={e => setFaces(e.target.value)} />-sided die <input type="number" min="1" value={times} onChange={e => setTimes(e.target.value)} /> {times === "1" ? "time" : "times"}.</p>
-      <button onClick={() => rollDice(parseInt(faces), parseInt(times))}>Roll Dice!</button>
+      <button onClick={() => setRolls(rollDice(parseInt(faces), parseInt(times)))}>Roll Dice!</button>
       <p>{rolls.join(', ')}</p>
     </div>
   );
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,11 @@
 export function generateRandomIntegerInclusive(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function rollDice(faces: number, times: number) {
+  let rolls = [];
+  for (let i = 0; i < times; i++) {
+    rolls.push(generateRandomIntegerInclusive(1, faces));
+  }
+  return rolls;
+}
