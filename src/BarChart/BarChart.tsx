a8de8aab13e6359b9f24f91b6005d83c4ad8fe83
David Herszenhaut <david.herszenhaut@gmail.com>
2021-07-18 22:52:09 -0400

    Second pass at building bar chart

--- src/BarChart/BarChart.tsx
+++ src/BarChart/BarChart.tsx
@@ -1,77 +1,61 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import * as d3 from "d3";
 import { aggregateData } from "../utils";
 
-// interface RollData {
-//   value: number;
-//   count: number;
-// }
+interface RollData {
+  value: number;
+  count: number;
+}
+
+// https://medium.com/react-courses/drawing-basic-charts-with-react-typescript-d3-part-i-line-area-and-bar-charts-cd159c62e18c
 
 function BarChart(props: { rolls: number[] }): ReactElement {
-  const data = aggregateData(props.rolls);
-  const width = 500;
-  const height = 500;
-  const margin = { top: 30, right: 0, bottom: 30, left: 40 };
-
-  const color = "steelblue";
-  const x = d3
-    .scaleBand()
-    .domain(data.map((x) => x.value.toString()))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.count)] as number[])
-    .nice()
-    .range([height - margin.bottom, margin.top]);
-  // function xAxis(g) {
-  //   return g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-  //     d3
-  //       .axisBottom(x)
-  //       .tickFormat((i) => data[i].value)
-  //       .tickSizeOuter(0)
-  //   );
-  // }
-  // function yAxis(g) {
-  //   return g
-  //     .attr("transform", `translate(${margin.left},0)`)
-  //     .call(d3.axisLeft(y).ticks(null))
-  //     .call((g) => g.select(".domain").remove())
-  //     .call((g) =>
-  //       g
-  //         .append("text")
-  //         .attr("x", -margin.left)
-  //         .attr("y", 10)
-  //         .attr("fill", "currentColor")
-  //         .attr("text-anchor", "start")
-  //         .text(data.y)
-  //     );
-  // }
-
-  function drawChart() {
+  useEffect(() => {
+    drawChart();
+  });
+  const drawChart = () => {
+    const data = aggregateData(props.rolls);
+    const width = 800 - 50 - 50; // width - left - right
+    const height = 400 - 10 - 50; // height - top - bottom
+    const x = d3.scaleBand().range([0, width]).padding(0.8);
+    const y = d3.scaleLinear().range([height, 0]);
     const svg = d3
       .select("#bar-chart")
-      .attr("viewBox", `${[0, 0, width, height]}`);
+      .attr("width", width + 50 + 50)
+      .attr("height", 400 + 10 + 50)
+      .append("g")
+      .attr("transform", `translate(${50},${10})`);
+    x.domain(data.map((d) => d.value.toString()));
+    y.domain([
+      0,
+      d3.max(data, () => {
+        return Math.max(...data.map((dt) => (dt as RollData).count), 0);
+      }),
+    ] as number[]);
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("fill", "#a3be8c")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.value.toString()) || 0)
+      .attr("width", x.bandwidth())
+      .attr("y", (d) => {
+        return y(d.count) || d.count;
+      })
+      .attr("height", (d) => {
+        return height - (y(d.value) || d.value);
+      });
     svg
       .append("g")
-      .attr("fill", color)
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("x", (d, i) => x(i.toString()) || 0)
-      .attr("y", (d) => y(d.count) || 0)
-      .attr("height", (d) => y(d.count) || 0)
-      .attr("width", x.bandwidth());
-    svg.append("g").call(d3.axisBottom(x));
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(x));
     svg.append("g").call(d3.axisLeft(y));
-    return svg.node();
-  }
-
-  drawChart();
-
+  };
   return (
     <div>
-      <svg height={height} width={width} id="bar-chart"></svg>
+      <svg id="bar-chart"></svg>
     </div>
   );
 }
